e0c33db1999e1f975a20cb6aae51a46eb8019c4f
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-06-25 01:14:36 -0400

    clean build

--- AirLineTicketing/obj/Debug/net8.0-windows/AirLineTicketing.AssemblyInfo.cs
+++ AirLineTicketing/obj/Debug/net8.0-windows/AirLineTicketing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirLineTicketing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7781fc391b38b432fac489fc427a43b6537d8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9f069f21fcd8d70d1197bde055cd3ecf8fd7ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirLineTicketing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirLineTicketing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
