3299d0111da9138e5a620bfaf7edf647704ff6f7
Marie Andrews <marie.andrews2222@gmail.com>
2024-06-28 11:14:43 -0400

    Created SelectedProduct.cs, inheriting from Product.cs

---
+++ Assignment1/SelectedProduct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Assignment1_FarmersMarketApp
+{
+    internal class SelectedProduct: Product
+    {
+        private int amountSelected;
+
+        public SelectedProduct() : base()
+        {
+            this.amountSelected = 0;
+        }
+
+        public SelectedProduct(string name, int id, double amount, double price, int amountSelected) : base(name, id, amount, price)
+        {
+            this.amountSelected = amountSelected;
+        }
+
+        public int getAmountSelected()
+        {
+            return this.amountSelected;
+        }
+
+        public void setAmountSelected(int amountSelected)
+        {
+            this.amountSelected = amountSelected;
+        }
+    }
+}
