7b276928fa5f96b0fcf7459c8384b7e1932bff49
Marie Andrews <marie.andrews2222@gmail.com>
2024-06-28 13:18:00 -0400

    Populating comboBox of available products

--- Assignment1/Sales.xaml.cs
+++ Assignment1/Sales.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,9 +20,37 @@ namespace Assignment1_FarmersMarketApp
     /// </summary>
     public partial class Sales : Window
     {
+        private ApiRequest apiRequest;
+
         public Sales()
         {
             InitializeComponent();
+            apiRequest = new ApiRequest();
+            PopulateSelectionComboBox();
+            List<SelectedProduct> selectedProductList = new List<SelectedProduct>();
+        }
+
+        private void AddProductBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void PopulateSelectionComboBox()
+        {
+            System.Object[] availableProduct = new System.Object[5];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                availableProduct[i] = "Item" + i;
+            }
+            productComboBox.Items.AddRange(availableProduct);
         }
     }
 }
