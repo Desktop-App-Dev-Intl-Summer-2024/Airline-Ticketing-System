4dd72c92396a6453ad6d74300816583152884d14
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-07-09 23:27:26 -0400

    bootstrap assignment 2
    Create REST APIs for Insert and Update Products for client application

---
+++ Assignment2/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private static String server_env_var_name = "DEV_AIRLINE_TICKETING_APP_SERVER";
+        private static String local_server_name = Environment.GetEnvironmentVariable(server_env_var_name);
+
+        private readonly IConfiguration _configuration;
+        private SqlConnection con;
+        private DatabaseApp app;
+
+        public ProductController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            app = new DatabaseApp();
+
+            if (String.IsNullOrEmpty(local_server_name))
+            {
+                Console.WriteLine("Please set environment variable: " + server_env_var_name);
+            }
+
+            string connectionString = "Data Source=" + local_server_name + ";Initial Catalog=AirlineTicketingAppProjet;Integrated Security=True;Trust Server Certificate=True";
+
+            con = new SqlConnection(connectionString);
+        }
+
+        [HttpPost]
+        [Route("AddProduct")]
+
+        public Response AddProduct(Product product)
+        {
+            return app.AddProduct(con, product);
+        }
+
+        [HttpPut]
+        [Route("UpdateProductById/{id}")]
+
+        public Response UpdateProductById(int id, Product product)
+        {
+            return app.UpdateProductById(con, id, product);
+        }
+    }
+}
---
+++ Assignment2/Models/DatabaseApp.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Assignment2.Models
+{
+    public class DatabaseApp
+    {
+        public Response AddProduct(SqlConnection sqlCon, Product product)
+        {
+            Response response = new Response();
+
+            try
+            {
+                string query = "insert into A1Products values(@id, @name, @amount, @price)";
+
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+
+                cmd.Parameters.AddWithValue("@name", product.name);
+                cmd.Parameters.AddWithValue("@id", product.id);
+                cmd.Parameters.AddWithValue("@amount", product.amount);
+                cmd.Parameters.AddWithValue("@price", product.price);
+
+                sqlCon.Open();
+
+                int status = cmd.ExecuteNonQuery();
+
+                if (status == 1)
+                {
+                    response.statusCode = 200;
+                    response.statusMessage = "Product added successfully!";
+                    response.product = product;
+                    response.products = null;
+                }
+                else
+                {
+                    response.statusCode = 100;
+                    response.statusMessage = "Product couldn't be added, Check logs!";
+                    response.product = null;
+                    response.products = null;
+                }
+
+                sqlCon.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return response;
+        }
+
+        public Response UpdateProductById(SqlConnection con, int id, Product product)
+        {
+            Response response = new Response();
+
+            try
+            {
+                con.Open();
+
+                string query = "update A1Products set name=@name, amount=@amount, price=@price where id=@id";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@name", product.name);
+                cmd.Parameters.AddWithValue("@amount", product.amount);
+                cmd.Parameters.AddWithValue("@price", product.price);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int i = cmd.ExecuteNonQuery();
+
+                if (i > 0)
+                {
+                    response.statusCode = 200;
+                    response.statusMessage = "Product updated successfully!";
+                    response.product = product;
+                    response.products = null;
+                }
+                else
+                {
+                    response.statusCode = 100;
+                    response.statusMessage = "Student couldn't be updated";
+                    response.product = null;
+                    response.products = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            con.Close();
+
+            return response;
+        }
+    }
+}
