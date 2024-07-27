4c60fa7db95573421d9b14ca6c16aa66b36dc618
Marie Andrews <marie.andrews2222@gmail.com>
2024-07-27 16:01:36 -0400

    Added files from Lab with some updates

---
+++ Assignment3/Marie/TestShippingApp/TestShippingApp/UnitTest1.cs
@@ -0,0 +1,19 @@
+
+
+namespace TestShippingApp
+{
+    public class Tests
+    {
+      
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
