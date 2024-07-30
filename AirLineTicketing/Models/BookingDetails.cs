521cdd738c1f80901c57c832e73c983b5f79cc18
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-07-30 03:49:59 -0400

    add form logic to collect data

---
+++ AirLineTicketing/Models/BookingDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineTicketing.Models
+{
+    public class BookingDetails
+    {
+        public string firstName { get; set; }
+        public string lastName {  get; set; }
+        public string classType { get; set; }
+        public string nationality { get; set; }
+        public string passportNumber { get; set; }
+        public string address { get; set; }
+        public string cardNumber { get; set; }
+        public string expiryDate { get; set; }
+        public string cardCvv { get; set; }
+        public string bookingDateTime { get; set; }
+        public double ticketCost { get; set; }
+        public int flightNo { get; set; }
+    }
+}
--- AirLineTicketing/Views/Booking.xaml.cs
+++ AirLineTicketing/Views/Booking.xaml.cs
@@ -21,9 +21,13 @@ namespace AirLineTicketing.Views
     /// </summary>
     public partial class Booking : Window
     {
+        Flight selectedFlight;
+
         public Booking()
         {
             InitializeComponent();
+
+            selectedFlight = MainWindow.selectedFlight;
 
             setHeading();
             initializeGrid();
@@ -32,7 +36,7 @@ namespace AirLineTicketing.Views
         }
 
         private void initializeComboBoxes() {
-            ClassCmbBx.ItemsSource = MainWindow.selectedFlight.availableClasses.Split(' ');
+            ClassCmbBx.ItemsSource = selectedFlight.availableClasses.Split(' ');
         }
 
         private void initializePrice() { 
@@ -45,8 +49,6 @@ namespace AirLineTicketing.Views
         }
 
         private void initializeGrid() {
-            Flight? selectedFlight = MainWindow.selectedFlight;
-
             List<Flight> list = [selectedFlight];
 
             if (selectedFlight != null)
@@ -63,6 +65,98 @@ namespace AirLineTicketing.Views
             User user = MainWindow.user;
 
             HeadingLabel.Content = "Welcome, " + user.getFirstname() + " " + user.getLastname();
+        }
+
+        private void FirstnameTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FirstnameTxtBx.Text = "";
+        }
+
+        private void LastNameTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            LastNameTxtBx.Text = "";
+        }
+
+        private void NationalityTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            NationalityTxtBx.Text = "";
+        }
+
+        private void PassportNoTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PassportNoTxtBx.Text = "";
+        }
+
+        private void AddressTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AddressTxtBx.Text = "";
+        }
+
+        private void CvvTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            CvvTxtBx.Text = "";
+        }
+
+        private void CardExpDatePckr_GotFocus(object sender, RoutedEventArgs e)
+        {
+            CardExpDatePckr.Text = "";
+        }
+
+        private void CardNoTxtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            CardNoTxtBx.Text = "";
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ConfirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string firstName = FirstnameTxtBx.Text.Trim();
+                string lastName = LastNameTxtBx.Text.Trim();
+                string? classType = ClassCmbBx.SelectedItem.ToString();
+                string nationality = NationalityTxtBx.Text.Trim();
+                string passportNumber = PassportNoTxtBx.Text.Trim();
+                string address = AddressTxtBx.Text.Trim();
+                string cardNumber = CardNoTxtBx.Text.Trim();
+                string? expiryDate = CardExpDatePckr.Text;
+                string cardCvv = CvvTxtBx.Text.Trim();
+                string bookingDateTime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+                double ticketCost = Convert.ToDouble(TicketPriceDisplayLbl.Content.ToString().Substring(1));
+
+
+                int flightNo = selectedFlight.flightNo;
+
+                BookingDetails details = new BookingDetails();
+
+                details.firstName = firstName;
+                details.lastName = lastName;
+                details.classType = classType;
+                details.nationality = nationality;
+                details.passportNumber = passportNumber;
+                details.address = address;
+                details.cardNumber = cardNumber;
+                details.expiryDate = expiryDate;
+                details.cardCvv = cardCvv;
+                details.bookingDateTime = bookingDateTime;
+                details.ticketCost = ticketCost;
+                details.flightNo = flightNo;
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User? user = MainWindow.user;
+
+            FirstnameTxtBx.Text = user?.getFirstname();
+            LastNameTxtBx.Text = user?.getLastname();
         }
     }
 }
